3cd7309e399db1e83f34c11ae0b279b481fee105
Viktor Danilov <wikslayer@gmail.com>
2019-05-31 23:13:25 +0300

    Добавил задание 1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-console.log('Hi');
+import { task1Old, task1New } from './es-features/task1';
+
+console.log('task1 old', task1Old(10) === 500); // true
+console.log('task1 new', task1New(10) === 500); // true
+
+console.log('=============');
