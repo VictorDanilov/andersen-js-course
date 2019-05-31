66e3e6819f296881252c17a05b78b9385bbeab81
volvachev <e.volvachev@andersenlab.com>
2019-06-01 00:52:22 +0300

    Добавил задачи по массивам.

---
+++ src/array-practice/task2.js
@@ -0,0 +1,5 @@
+/**
+ * Реализовать функцию arrayDiff
+ * console.log(arrayDiff([1, 2, 3], [1, 2, 4])); -> [3, 4]
+ * console.log(arrayDiff([1, 3, 3, 4], [1, 3, '4'])); -> [4, '4']
+ */
---
+++ src/array-practice/task5.js
@@ -0,0 +1,10 @@
+/**
+ * Реализовать функцию createGenerator
+ * const generator = createGenerator([1, '6', 3, 2]);
+ * generator.next(); -> 1
+ * generator.next(); -> '6'
+ * generator.next(); -> 3
+ * generator.next(); -> 2
+ * generator.next(); -> 'Complete!'
+ * generator.next(); -> 'Complete!'
+ */
