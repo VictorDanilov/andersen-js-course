3539934ea262f46ad00929420f5a9c4b792018dd
Viktor Danilov <wikslayer@gmail.com>
2019-05-31 21:37:08 +0300

    Добавил webpack

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require("path");
+
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist")
+  },
+  devServer: {
+    contentBase: "./dist"
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "JS Course"
+    })
+  ]
+};
