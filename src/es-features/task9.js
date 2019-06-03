32e010fa51786aef557302a0402a9f50f1833811
sivtsova <example@email.com>
2019-06-03 20:15:34 +0300

    Adding task9.

--- src/es-features/task9.js
+++ src/es-features/task9.js
@@ -6,15 +6,20 @@
  */
 
 export function task9Old() {
-  var result = 'test'.indexOf('est') !== -1;
-  var findResult = [22, 1, 90, 70].filter(function(x) {
-    return x < 3;
-  })[0];
+    var result = 'test'.indexOf('est') !== -1;
+    var findResult = [22, 1, 90, 70].filter(function (x) {
+        return x < 3;
+    })[0];
 
-  return [result, findResult];
+    return [result, findResult];
 }
 
 // Напишите реализацию функции task9Old на ES6+ ниже этого комментария.
 // При желании, можете использовать стрелочную функцию, вместо обычной
 
-export function task9New() {}
+export function task9New() {
+    var result = 'test'.indexOf('est') !== -1;
+    var findResult = [22, 1, 90, 70].filter(x => x < 3)[0];
+
+    return [result, findResult];
+}
