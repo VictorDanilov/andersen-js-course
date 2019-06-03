173cf5525d6c5777d0c673722b015841e56b9b88
sivtsova <example@email.com>
2019-06-03 20:58:08 +0300

    Adding task10.

--- src/es-features/task10.js
+++ src/es-features/task10.js
@@ -3,50 +3,50 @@
  */
 
 function A(name) {
-  this.name = name;
+    this.name = name;
 }
 
-A.prototype.getName = function() {
-  return this.name;
+A.prototype.getName = function () {
+    return this.name;
 };
 
 function B(name, age) {
-  A.call(this, name);
-  this.age = age;
+    A.call(this, name);
+    this.age = age;
 }
 
 B.prototype = Object.create(A.prototype);
 B.prototype.constructor = B;
 
-B.prototype.getName = function(text) {
-  return A.prototype.getName.call(this) + ' ' + text;
+B.prototype.getName = function (text) {
+    return A.prototype.getName.call(this) + ' ' + text;
 };
 
-B.prototype.getAge = function() {
-  return this.age;
+B.prototype.getAge = function () {
+    return this.age;
 };
 
-B.defaultUser = function() {
-  return new B('test', 0);
+B.defaultUser = function () {
+    return new B('test', 0);
 };
 
 Object.defineProperty(B.prototype, 'color', {
-  get: function() {
-    return this._color;
-  },
-  set: function(color) {
-    this._color = color;
-  },
+    get: function () {
+        return this._color;
+    },
+    set: function (color) {
+        this._color = color;
+    }
 });
 
 export function task10Old() {
-  var b = new B('Max', 12);
-  console.log(b.getName('Best')); // Max Best
-  console.log(b.getAge()); // 12
-  console.log(B.defaultUser()); // {name: 'test', age: 0}
-  b.color = 'red';
-  console.log(b.color); // red
-  return b;
+    var b = new B('Max', 12);
+    console.log(b.getName('Best')); // Max Best
+    console.log(b.getAge()); // 12
+    console.log(B.defaultUser()); // {name: 'test', age: 0}
+    b.color = 'red';
+    console.log(b.color); // red
+    return b;
 }
 
 // Перепишите функции-конструкторы A и B ниже этого комментария.
@@ -54,4 +54,51 @@ export function task10Old() {
 // Изменить тело функции task10Old, написав task10New, где заиспользуете
 // класс так, как использовалась функция-конструктор B в task10Old
 
-export function task10New() {}
+class NewA {
+
+    constructor(name) {
+        this.name = name;
+    }
+
+    getName() {
+        return this.name;
+    }
+}
+
+class NewB extends NewA {
+
+    constructor(name, age) {
+        super(name);
+        this.age = age;
+    }
+
+    getName(text) {
+        return super.getName() + ' ' + text;
+    }
+
+    getAge() {
+        return this.age;
+    }
+
+    static defaultUser() {
+        return new NewB('test', 0);
+    }
+
+    set color(color) {
+        this._color = color;
+    }
+
+    get color() {
+        return this._color;
+    }
+}
+
+export function task10New() {
+    var b = new NewB('Max', 12);
+    console.log(b.getName('Best')); // Max Best
+    console.log(b.getAge()); // 12
+    console.log(NewB.defaultUser()); // {name: 'test', age: 0}
+    b.color = 'red';
+    console.log(b.color); // red
+    return b;
+}
