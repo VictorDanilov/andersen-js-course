29f7e1d12bc63490a96b7e1ad93fbde6b03dac57
sivtsova <example@email.com>
2019-06-04 22:29:27 +0300

    Array-practice. Adding task10.

--- src/array-practice/task10.js
+++ src/array-practice/task10.js
@@ -11,10 +11,17 @@
 // Раскомментировать нижнее, при начале реализации
 
 // const meetups = [
-//   { name: 'JavaScript', isActive: true, members: 100 },
-//   { name: 'Angular', isActive: true, members: 900 },
-//   { name: 'Node', isActive: false, members: 600 },
-//   { name: 'React', isActive: true, members: 500 },
+//     {name: 'JavaScript', isActive: true, members: 100},
+//     {name: 'Angular', isActive: true, members: 900},
+//     {name: 'Node', isActive: false, members: 600},
+//     {name: 'React', isActive: true, members: 500},
 // ];
 
-// function membersOnActiveMeetups(meetups) {}
+export function membersOnActiveMeetups(meetups) {
+    return meetups.reduce((previousValue, item) => {
+        if (item.isActive) {
+            previousValue += item.members;
+        }
+        return previousValue;
+    }, 0);
+}
