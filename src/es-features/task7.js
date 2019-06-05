6018f9ae483113649595bfeb09ede9cb44679470
rick-nice <rickman.nice@gmail.com>
2019-06-05 15:11:28 +0300

    Seventh task done.

--- src/es-features/task7.js
+++ src/es-features/task7.js
@@ -21,4 +21,8 @@ export function task7Old() {
 // Напишите реализацию функции task7Old на ES6+ ниже этого комментария.
 // При желании, можете использовать стрелочную функцию, вместо обычной
 
-export function task7New() {}
+export const task7New = () => {
+  const obj = { test: 0 };
+  const obj2 = { foo: 1, bar: 2 };
+  return Object.assign(obj, obj2);
+};
