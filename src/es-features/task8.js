ce7c849ddbc95734687bc248efc37d37d99fb0e6
xengus <serjiox13@gmail.com>
2019-06-05 22:52:01 +0300

    Task 8 done

--- src/es-features/task8.js
+++ src/es-features/task8.js
@@ -28,4 +28,14 @@ export function task8Old() {
 // Напишите реализацию функции task8Old на ES6+ ниже этого комментария.
 // При желании, можете использовать стрелочную функцию, вместо обычной
 
-export function task8New() {}
+function task8Old() {
+  const obj = {
+    nums: [0, 2, 5, 10, 15],
+    fives: [],
+    calculateNumsFive() {
+           this.nums.filter(function(v) { 
+		(v % 5 === 0)? this.fives.push(v)});
+		    };
+
+  return obj.calculateNumsFive()
+}
