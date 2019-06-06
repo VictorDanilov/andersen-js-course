166b4c9e3cdf131c36d9d0701969036ac539dc1b
rick-nice <rickman.nice@gmail.com>
2019-06-06 15:01:25 +0300

    Fourth task from array-practice done.

--- src/array-practice/task4.js
+++ src/array-practice/task4.js
@@ -7,3 +7,7 @@
  * console.log(union([5, 1, 2, 3, 3], [4, 3, 2])); -> [5, 1, 2, 3, 4]
  * console.log(union([5, 1, 3, 3, 4], [1, 3, 4])); -> [5, 1, 3, 4]
  */
+
+const union = (firstArray, secondArray) => [...new Set([...firstArray, ...secondArray])];
+
+export default union;
