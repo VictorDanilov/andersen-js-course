10b5214e4211318d03736a80f6740156d747b345
rick-nice <rickman.nice@gmail.com>
2019-06-06 14:53:02 +0300

    Third task from array-practice done.

--- src/array-practice/task3.js
+++ src/array-practice/task3.js
@@ -9,3 +9,11 @@
  *
  * forEachRight([1, 2, 3, 4], val => console.log(val)); -> в консоль 4 3 2 1
  */
+
+const forEachRight = (array, cb) => {
+  array.forEach((elem, index) => {
+    cb(array[array.length - index - 1]);
+  });
+};
+
+export default forEachRight;
