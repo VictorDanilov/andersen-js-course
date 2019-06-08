bb3fdbd18e5bee1e1b41de027baba70bc0293da0
xengus <serjiox13@gmail.com>
2019-06-08 15:47:31 +0300

    Task 6 done

--- src/array-practice/task6.js
+++ src/array-practice/task6.js
@@ -20,3 +20,8 @@
  * console.log(transformArrayToNumber([10, 20, 30], (acc, item) => acc * item), 1); -> 6000
  * console.log(transformArrayToNumber([10, 20, 30], (acc, item) => acc - item)); -> -60
  */
+function transformArrayToNumber(arr,fun = (acc, item) =>!!(acc,item), c = 0) {
+  return arr.reduce(fun, c);
+}
+
+const transformArrayToNumber = (arr,fun = (acc, item) =>!!(acc,item), c = 0) => return arr.reduce(fun, c);
