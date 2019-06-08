e16db5e45196b50507b67a7e7cbdd8893b40586e
Natada122 <bynatada122@gmail.com>
2019-06-08 21:32:44 +0300

    8 задание,массивы

--- src/array-practice/task8.js
+++ src/array-practice/task8.js
@@ -11,3 +11,8 @@
  * console.log(without([2, 1, 2, 3], 1, 2)) -> [3]
  * console.log(without([2, 1, 10, 20], 1, 2)) -> [10, 20]
  */
+function without(arr, ...x) {
+  return arr.filter(value => !x.includes(value));
+}
+console.log(without([2, 1, 2, 3], 1, 2));
+console.log(without([2, 1, 10, 20], 1, 2));
