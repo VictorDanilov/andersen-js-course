47efd6b2d42fd067e74b35d0e19e12f5741a039f
Natada122 <bynatada122@gmail.com>
2019-06-08 21:33:41 +0300

    10 задание,массивы

--- src/array-practice/task10.js
+++ src/array-practice/task10.js
@@ -9,12 +9,16 @@
  */
 
 // Раскомментировать нижнее, при начале реализации
+const meetups = [
+  { name: 'JavaScript', isActive: true, members: 100 },
+  { name: 'Angular', isActive: true, members: 900 },
+  { name: 'Node', isActive: false, members: 600 },
+  { name: 'React', isActive: true, members: 500 },
+];
 
-// const meetups = [
-//   { name: 'JavaScript', isActive: true, members: 100 },
-//   { name: 'Angular', isActive: true, members: 900 },
-//   { name: 'Node', isActive: false, members: 600 },
-//   { name: 'React', isActive: true, members: 500 },
-// ];
-
-// function membersOnActiveMeetups(meetups) {}
+function membersOnActiveMeetups(meetups) {
+  return meetups.reduce(function(count, current) {
+    return current.isActive ? count + current.members : count;
+  }, 0);
+}
+console.log(membersOnActiveMeetups(meetups));
