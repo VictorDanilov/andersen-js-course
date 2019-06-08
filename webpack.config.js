714a8b1950e26aa79733128898568db9242e33b0
Viktor Danilov <wikslayer@gmail.com>
2019-06-08 14:29:22 +0300

    Добавил eslint-loader

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,7 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: ['babel-loader'],
+        use: ['babel-loader', 'eslint-loader'],
       },
       {
         test: /\.(svg|png|gif|jpg|webp)$/,
