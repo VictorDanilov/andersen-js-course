7d60118f09b75fafff0df5913844c4ea816a824e
Natada122 <bynatada122@gmail.com>
2019-06-09 20:53:41 +0300

    7 задание,ES6

--- src/es-features/task7.js
+++ src/es-features/task7.js
@@ -21,4 +21,8 @@ export function task7Old() {
 // Напишите реализацию функции task7Old на ES6+ ниже этого комментария.
 // При желании, можете использовать стрелочную функцию, вместо обычной
 
-export function task7New() {}
+export function task7New() {
+  const obj = { test: 0 };
+  const obj2 = { foo: 1, bar: 2 };
+  return { ...obj, ...obj2 };
+}
