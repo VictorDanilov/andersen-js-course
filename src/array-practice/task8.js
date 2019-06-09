fee2da2c083edea800a7d9fd2cddb22cdeefc867
xengus <serjiox13@gmail.com>
2019-06-09 19:35:52 +0300

    Task 8 done

--- src/array-practice/task8.js
+++ src/array-practice/task8.js
@@ -11,3 +11,6 @@
  * console.log(without([2, 1, 2, 3], 1, 2)) -> [3]
  * console.log(without([2, 1, 10, 20], 1, 2)) -> [10, 20]
  */
+function without (arr, ...numbers) {
+  return arr.filter(elem => numbers.indexOf(elem) === -1);
+}
