78e2d957dd349ecdfd42da9b4569974abe912c66
5tronciy <romangolchuk@gmail.com>
2021-07-30 17:27:38 +0300

    add task5

--- src/es-features/task5.js
+++ src/es-features/task5.js
@@ -16,4 +16,6 @@ export function task5Old(array, obj) {
 // Напишите реализацию функции task5Old на ES6+ ниже этого комментария.
 // При желании, можете использовать стрелочную функцию, вместо обычной
 
-export function task5New() {}
+export function task5New(array, obj) {
+  return [...array, ...Object.values(obj)];
+}
