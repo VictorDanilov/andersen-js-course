3c62451ce06bd7899937aa4cafb59defb09cb338
5tronciy <romangolchuk@gmail.com>
2021-07-30 17:30:39 +0300

    add task2

--- src/array-practice/task2.js
+++ src/array-practice/task2.js
@@ -15,3 +15,10 @@
  * console.log(arrayDiff([1, 2, 3], [1, 2, 4])); -> [3, 4]
  * console.log(arrayDiff([1, 3, 3, 4], [1, 3, '4'])); -> [4, '4']
  */
+
+export default function arrayDiff(arr1, arr2) {
+  return [
+    ...arr1.filter(item => !arr2.includes(item)),
+    ...arr2.filter(item => !arr1.includes(item)),
+  ];
+}
