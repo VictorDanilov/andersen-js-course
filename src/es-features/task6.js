911517edbed48c3dba03e87dfb4f9eacf196310b
5tronciy <romangolchuk@gmail.com>
2021-07-30 17:27:57 +0300

    add task6

--- src/es-features/task6.js
+++ src/es-features/task6.js
@@ -28,4 +28,19 @@ export function task6Old() {
 // Напишите реализацию функции task6Old на ES6+ ниже этого комментария.
 // При желании, можете использовать стрелочную функцию, вместо обычной
 
-export function task6New() {}
+export function task6New() {
+  const userModule = () => ({
+    name: 'Max',
+    value: 100,
+    role: { name: 'Admin' },
+    cases: [{ id: '1' }],
+  });
+
+  const tmp = userModule();
+
+  const { name, value, role, cases } = tmp;
+  const isActive = tmp.isActive || false;
+  const firstCaseId = cases[0].id;
+
+  return [name, value, role.name, isActive, firstCaseId];
+}
