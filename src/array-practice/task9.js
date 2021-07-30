409de20c35c9068aebcce9486881b8b0043d16d1
5tronciy <romangolchuk@gmail.com>
2021-07-30 17:32:31 +0300

    add task9

--- src/array-practice/task9.js
+++ src/array-practice/task9.js
@@ -10,3 +10,7 @@
  * console.log(indexOfAll([1, 2, 3, 1, 2, 3], 1)); -> [0, 3]
  * console.log(indexOfAll([1, 2, 3], 4)); -> []
  */
+
+export default function indexOfAll(arr, value) {
+  return arr.reduce((acc, item, index) => (item === value ? acc.push(index) : acc), []);
+}
