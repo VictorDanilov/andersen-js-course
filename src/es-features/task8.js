f82d68e29bd5c84c648a838c1cbba3919854b43b
5tronciy <romangolchuk@gmail.com>
2021-07-30 17:28:22 +0300

    add task8

--- src/es-features/task8.js
+++ src/es-features/task8.js
@@ -28,4 +28,14 @@ export function task8Old() {
 // Напишите реализацию функции task8Old на ES6+ ниже этого комментария.
 // При желании, можете использовать стрелочную функцию, вместо обычной
 
-export function task8New() {}
+export function task8New() {
+  const obj = {
+    nums: [0, 2, 5, 10, 15],
+    fives: [],
+    calculateNumsFive() {
+      return this.nums.filter(v => v % 5 === 0);
+    },
+  };
+
+  return obj.calculateNumsFive();
+}
