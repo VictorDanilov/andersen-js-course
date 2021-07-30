131cb8f1154d05d3c45e6984248d5f1d17e798b5
5tronciy <romangolchuk@gmail.com>
2021-07-30 17:27:08 +0300

    add task3

--- src/es-features/task3.js
+++ src/es-features/task3.js
@@ -14,4 +14,6 @@ export function task3Old(params) {
 // Напишите реализацию функции task3Old на ES6+ ниже этого комментария.
 // При желании, можете использовать стрелочную функцию, вместо обычной
 
-export function task3New() {}
+export function task3New(params) {
+  return [1, 2, ...params];
+}
