1fe61354755a4af8fc38955f051d4879b940b30b
5tronciy <romangolchuk@gmail.com>
2021-07-30 17:32:04 +0300

    add task7

--- src/array-practice/task7.js
+++ src/array-practice/task7.js
@@ -11,3 +11,7 @@
  * console.log(take([1, 2, 3], 5)); -> [1, 2, 3]
  * console.log(take([1, 2, 3], 2)); -> [1, 2]
  */
+
+export default function take(arr, value) {
+  return arr.slice(0, value);
+}
