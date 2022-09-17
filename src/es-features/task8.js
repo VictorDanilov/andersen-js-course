01c5db60a5b633a82f8fe28f62648ad1dc49146b
Vgyand <gyandzhunczevv@gmail.com>
2022-09-17 16:11:31 +0300

    Es6Task8

--- src/es-features/task8.js
+++ src/es-features/task8.js
@@ -10,9 +10,9 @@ export function task8Old() {
   var obj = {
     nums: [0, 2, 5, 10, 15],
     fives: [],
-    calculateNumsFive: function() {
+    calculateNumsFive: function () {
       var self = this;
-      this.nums.forEach(function(v) {
+      this.nums.forEach(function (v) {
         if (v % 5 === 0) {
           self.fives.push(v);
         }
@@ -28,4 +28,12 @@ export function task8Old() {
 // Напишите реализацию функции task8Old на ES6+ ниже этого комментария.
 // При желании, можете использовать стрелочную функцию, вместо обычной
 
-export function task8New() {}
+export const task8New = () => {
+  const obj = {
+    nums: [0, 2, 5, 10, 15],
+    calculateNumsFive: function () {
+      return this.nums.filter(el => el % 5 === 0);
+    },
+  };
+  return obj.calculateNumsFive();
+};
--- src/index.js
+++ src/index.js
@@ -16,60 +16,60 @@
  * Для задач на array-practice импортируйте написанные функции сюда и проверяйте их работу тут.
  */
 
-import { task1Old, task1New } from './es-features/task1';
-import { task2Old, task2New } from './es-features/task2';
-import { task3Old, task3New } from './es-features/task3';
-import { task4Old, task4New } from './es-features/task4';
-import { task5Old, task5New } from './es-features/task5';
-import { task6Old, task6New } from './es-features/task6';
-import { task7Old, task7New } from './es-features/task7';
-// import { task8Old, task8New } from './es-features/task8';
+// import { task1Old, task1New } from './es-features/task1';
+// import { task2Old, task2New } from './es-features/task2';
+// import { task3Old, task3New } from './es-features/task3';
+// import { task4Old, task4New } from './es-features/task4';
+// import { task5Old, task5New } from './es-features/task5';
+// import { task6Old, task6New } from './es-features/task6';
+// import { task7Old, task7New } from './es-features/task7';
+import { task8Old, task8New } from './es-features/task8';
 // import { task9Old, task9New } from './es-features/task9';
 // import { task10Old, task10New } from './es-features/task10';
-import { task11Old, task11New } from './es-features/task11';
+// import { task11Old, task11New } from './es-features/task11';
 // import { task12Old, task12New } from './es-features/task12';
 // import { task13Old, task13New } from './es-features/task13';
 // import { task14Old, task14New } from './es-features/task14';
 // import { task15Old, task15New } from './es-features/task15';
 
 
-console.log('task1 old', task1Old(10) === 500); // true
-console.log('task1 new', task1New(10) === 500); // true
-
-console.log('=============');
-
-console.log('task2 old', task2Old(1, 2, 'test', false, 'new', 'best') === 12); // true
-console.log('task2 new', task2New(1, 2, 'test', false, 'new', 'best') === 12); // true
-
-console.log('=============');
-
-console.log('task3 old', task3Old(['hello', 'test', 12])); // [ 1, 2, 'hello', 'test', 12 ]
-console.log('task3 new', task3New(['hello', 'test', 12])); // [ 1, 2, 'hello', 'test', 12 ]
-
-console.log('=============');
-
-console.log('task4 old', task4Old()); // {x: 10, y: 10, bar: function, baztest: 'new field'}
-console.log('task4 new', task4New()); // {x: 10, y: 10, bar: function, baztest: 'new field'}
-
-console.log('=============');
-
-console.log('task5 old', task5Old(['test', 42], { name: 'foo', val: 7 })); // ['test', 42, 'foo', 7]
-console.log('task5 new', task5New(['test', 42], { name: 'foo', val: 7 })); // ['test', 42, 'foo', 7]
-
-console.log('=============');
-
-console.log('task6 old', task6Old()); // ['Max', 100, 'Admin', false, '1']
-console.log('task6 new', task6New()); // ['Max', 100, 'Admin', false, '1']
-
-console.log('=============');
-
-console.log('task7 old', task7Old()); // {test: 0, foo: 1, bar: 2}
-console.log('task7 new', task7New()); // {test: 0, foo: 1, bar: 2}
+// console.log('task1 old', task1Old(10) === 500); // true
+// console.log('task1 new', task1New(10) === 500); // true
 
 // console.log('=============');
 
-// console.log('task8 old', task8Old()); // [0, 5, 10, 15]
-// console.log('task8 new', task8New()); // [0, 5, 10, 15]
+// console.log('task2 old', task2Old(1, 2, 'test', false, 'new', 'best') === 12); // true
+// console.log('task2 new', task2New(1, 2, 'test', false, 'new', 'best') === 12); // true
+
+// console.log('=============');
+
+// console.log('task3 old', task3Old(['hello', 'test', 12])); // [ 1, 2, 'hello', 'test', 12 ]
+// console.log('task3 new', task3New(['hello', 'test', 12])); // [ 1, 2, 'hello', 'test', 12 ]
+
+// console.log('=============');
+
+// console.log('task4 old', task4Old()); // {x: 10, y: 10, bar: function, baztest: 'new field'}
+// console.log('task4 new', task4New()); // {x: 10, y: 10, bar: function, baztest: 'new field'}
+
+// console.log('=============');
+
+// console.log('task5 old', task5Old(['test', 42], { name: 'foo', val: 7 })); // ['test', 42, 'foo', 7]
+// console.log('task5 new', task5New(['test', 42], { name: 'foo', val: 7 })); // ['test', 42, 'foo', 7]
+
+// console.log('=============');
+
+// console.log('task6 old', task6Old()); // ['Max', 100, 'Admin', false, '1']
+// console.log('task6 new', task6New()); // ['Max', 100, 'Admin', false, '1']
+
+// console.log('=============');
+
+// console.log('task7 old', task7Old()); // {test: 0, foo: 1, bar: 2}
+// console.log('task7 new', task7New()); // {test: 0, foo: 1, bar: 2}
+
+// console.log('=============');
+
+console.log('task8 old', task8Old()); // [0, 5, 10, 15]
+console.log('task8 new', task8New()); // [0, 5, 10, 15]
 
 // console.log('=============');
 
@@ -83,8 +83,8 @@ console.log('task7 new', task7New()); // {test: 0, foo: 1, bar: 2}
 
 // console.log('=============');
 
-console.log('task11 old', task11Old({ a: null }, 'a')); // false
-console.log('task11 new', task11New({ a: null }, 'a')); // false
+// console.log('task11 old', task11Old({ a: null }, 'a')); // false
+// console.log('task11 new', task11New({ a: null }, 'a')); // false
 
 // console.log('=============');
 
